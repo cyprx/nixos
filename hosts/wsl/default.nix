37404ad44d57316d7d8a8414e0d4581a297a5306
CY <cyprx@outlook.com>
2026-05-13 10:00:18 +0000

    fix: wsl nixos

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -9,6 +9,7 @@
 
 {
   imports = [
+    # include NixOS-WSL modules
   ];
 
   wsl.enable = true;
@@ -19,11 +20,12 @@
   wsl.ssh-agent.enable = true;
   networking.hostName = "cywsl";
 
+  services.dbus.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).  
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 }
