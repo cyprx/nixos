23bf1b0f9bd6a9a346aef99fcdbb8bcba3caf41a
cyprx <cyprx@outlook.com>
2026-06-02 12:52:38 +0000

    fix: move ghostty to darwin only

--- home/darwin.nix
+++ home/darwin.nix
@@ -6,4 +6,19 @@
   home.packages = [
 
   ];
+
+  programs.ghostty = {
+    enable = true;
+    # Set to null if using the Homebrew cask to avoid build errors
+    package = null; 
+    
+    settings = {
+        theme = "black-metal";
+        font-family = "Noto Nerd Font";
+        font-size = 14;
+        window-decoration = true;
+        macos-titlebar-style = "tabs";
+        confirm-close-surface = true;
+    };
+  };
 }
